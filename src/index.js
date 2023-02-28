feb0b4e93732f7cf6f42b474758c97d05280d200
Bogdan Skochynskyi <Bogdan.Skochynskyi@skywindgroup.com>
2023-02-28 17:12:04 +0200

    Add a store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import "bulma/css/bulma.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./store"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
